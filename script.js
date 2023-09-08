03deb52b484d58a968588e02c4901275983a9e92
Fabiano <fabiano.ssantos79@gmail.com>
2023-09-07 21:09:46 -0300

    update system

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const longBtn = document.querySelector('.app__card-button--longo')
 const imageBanner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const btnComecar = document.querySelector('#start-pause')
 const inputMusica = document.querySelector('#alternar-musica')
 const musicaFundo = new Audio('/sons/luna-rise-part-one.mp3')
+const musicaIniciar = new Audio('/sons/play.wav')
+const musicaPausar = new Audio('/sons/pause.mp3')
+const musicaFim = new Audio('/sons/beep.mp3')
+
 musicaFundo.loop = true
+
+let tempoDecorrido = 5;
+let intervaloId = null;
 
 inputMusica.addEventListener('change', () => {
     if (musicaFundo.paused) {
@@ -67,4 +75,32 @@ function alterarContexto(contexto) {
             break;
     }
 
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorrido <= 0) {
+        musicaFim.play()
+        zerar()
+        alert('Tempo finalizado!')
+        return
+    }
+    tempoDecorrido -= 1
+    console.log('Temporizador: ' + tempoDecorrido);
+}
+
+btnComecar.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        musicaPausar.play()
+        zerar()
+        return
+    }
+    musicaIniciar.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    intervaloId = null
 }
