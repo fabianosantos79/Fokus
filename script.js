2fb08aa6da044f767761e4c449fbd1a6e517856e
Fabiano <fabiano.ssantos79@gmail.com>
2023-09-09 02:57:45 -0300

    finish project

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const imageBanner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
 const btnComecar = document.querySelector('#start-pause')
+const btnIniciarOuPausar = document.querySelector('#start-pause span')
+const tempoNaTela = document.querySelector('#timer')
 const inputMusica = document.querySelector('#alternar-musica')
 const musicaFundo = new Audio('/sons/luna-rise-part-one.mp3')
 const musicaIniciar = new Audio('/sons/play.wav')
 const musicaPausar = new Audio('/sons/pause.mp3')
 const musicaFim = new Audio('/sons/beep.mp3')
+const iconeBtn = document.querySelector('.app__card-primary-butto-icon')
 
 musicaFundo.loop = true
 
-let tempoDecorrido = 5;
+let tempoDecorrido = 1500;
 let intervaloId = null;
 
 inputMusica.addEventListener('change', () => {
@@ -26,16 +29,19 @@ inputMusica.addEventListener('change', () => {
 })
 
 focoBtn.addEventListener('click', () => {
+    tempoDecorrido = 1500
     alterarContexto('foco')
     focoBtn.classList.add('active')
 })
 
 shortBtn.addEventListener('click', () => {
+    tempoDecorrido = 300
     alterarContexto('descanso-curto')
     shortBtn.classList.add('active')
 })
 
 longBtn.addEventListener('click', () => {
+    tempoDecorrido = 900
     alterarContexto('descanso-longo')
     longBtn.classList.add('active')
 })
@@ -45,7 +51,7 @@ inputMusica.addEventListener('change', () => {
 })
 
 function alterarContexto(contexto) {
-
+    mostrarTempo()
     botoes.forEach((item) => {
         item.classList.remove('active')
     })
@@ -85,18 +91,22 @@ const contagemRegressiva = () => {
         return
     }
     tempoDecorrido -= 1
-    console.log('Temporizador: ' + tempoDecorrido);
+    mostrarTempo()
 }
 
 btnComecar.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
     if (intervaloId) {
+        iconeBtn.setAttribute('src', '/imagens/play_arrow.png')
         musicaPausar.play()
+        btnIniciarOuPausar.textContent = "Começar"
         zerar()
         return
     }
     musicaIniciar.play()
+    iconeBtn.setAttribute('src', '/imagens/pause.png')
+    btnIniciarOuPausar.textContent = "Pausar"
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
@@ -104,3 +114,11 @@ function zerar() {
     clearInterval(intervaloId)
     intervaloId = null
 }
+
+function mostrarTempo() {
+    const tempo = new Date(tempoDecorrido * 1000)
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', { minute: '2-digit', second: '2-digit' })
+    tempoNaTela.innerHTML = tempoFormatado
+}
+
+mostrarTempo()
